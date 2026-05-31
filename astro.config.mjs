c0d5aeffb3dd210ead19d7adc708ff3440834010
botchagalupeai <142898388+botchagalupeai@users.noreply.github.com>
2026-05-31 10:25:52 -0500

    Remove Wrangler Cloudflare Worker config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   site: "https://botchagalupe.ai",
   output: "static",
-  integrations: [sitemap()],
-  adapter: cloudflare()
+  integrations: [sitemap()]
 });
